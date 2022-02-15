0465cba4584c699ee0db32c6a2cf0d2b46bf4dd6
Sergii-Kostanets <sergii.kostanets@ua.nestle.com>
2022-02-15 10:34:18 +0200

    problem with css

--- src/index.php
+++ src/index.php
@@ -10,7 +10,32 @@
 </head>
 
 <body>
-    
+
+    <div class="container">
+
+        <div class="todo_list">
+
+            <h1>ToDoList</h1>
+
+            <div class="create-new-todo">
+
+                <input type="text" class="message" name="task" id="new-task" placeholder="Напишите задание">
+                <button class="add">Добавить</button>
+
+            </div>
+
+            <div class="wrapper">
+
+                <ul class="todo">
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </body>
 
 </html>
